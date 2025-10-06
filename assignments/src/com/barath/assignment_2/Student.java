5da1ec61632d38413dfbff45753ea7c1581223ec
BARATH KM <BARATH KM@BARATH-PC>
2025-10-06 13:43:33 +0530

    commit

---
+++ assignments/src/com/barath/assignment_2/Student.java
@@ -0,0 +1,11 @@
+package com.barath.assignment_2;
+
+public class Student {
+    // Default constructor
+    public Student() {
+        System.out.println("Student object is created");
+    }
+}
+
+
+	
